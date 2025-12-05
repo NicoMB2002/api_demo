cc66cd80f17df57d66e2bc0cf4c60cfbfedb8734
nico <158498957+NicoMB2002@users.noreply.github.com>
2025-12-04 21:35:05 -0500

    Added api_practice

--- main.dart
+++ main.dart
@@ -1,84 +1,97 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'user_model.dart';
-import 'constants.dart';
-import 'api_service.dart';
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: APIDemo());
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const WeatherScreen(),
+    );
   }
 }
 
-class APIDemo extends StatefulWidget {
-  const APIDemo({super.key});
+class WeatherScreen extends StatefulWidget {
+  const WeatherScreen({super.key});
 
   @override
-  State<APIDemo> createState() => _APIDemoState();
+  State<WeatherScreen> createState() => _WeatherScreenState();
 }
 
-class _APIDemoState extends State<APIDemo> {
-  List<UserModel>? _userModel = []; //might be null now '?'
+class _WeatherScreenState extends State<WeatherScreen> {
+  final String apiKey = '6c5ad41d1500473170f259761f53f96c';
+  final double latitude = 45.5019; // Example: Montreal
+  final double longitude = -73.5674;
+
+  Map<String, dynamic>? weatherData;
+  bool isLoading = true;
 
   @override
   void initState() {
-    //TODO: implement initState
     super.initState();
-    //write a function to get the data
-    _getData();
+    fetchWeather();
   }
 
-  void _getData() async {
-    _userModel = (await ApiService()
-        .getUsers())!; //assert that this object is not null at that instance
-    Future.delayed(Duration(seconds: 2)).then((value) => setState(() {}));
+  Future<void> fetchWeather() async {
+    final url =
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        weatherData = json.decode(response.body);
+        isLoading = false;
+      });
+    } else {
+      throw Exception('Failed to load weather data');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Listing users from REST API')),
-      body: _userModel == null || _userModel!.isEmpty ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: _userModel!.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(_userModel![index].id.toString()),
-                          Text(_userModel![index].name),
-                        ],
-                      ),
-                      SizedBox(height: 20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(_userModel![index].email),
-                          Text(_userModel![index].website),
-                          Text(_userModel![index].address.street),
-                          Text(_userModel![index].company.name),
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              },
-          ),
+      appBar: AppBar(title: const Text('Current Weather')),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '${weatherData!['name']}',
+              style: const TextStyle(
+                  fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Lat: ${weatherData!['coord']['lat']}, Lon: ${weatherData!['coord']['lon']}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 10),
+            Image.network(
+              'https://openweathermap.org/img/wn/${weatherData!['weather'][0]['icon']}@2x.png',
+            ),
+            Text(
+              '${weatherData!['weather'][0]['main']} - ${weatherData!['weather'][0]['description']}',
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              '${weatherData!['main']['temp']} °C',
+              style: const TextStyle(fontSize: 24),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
